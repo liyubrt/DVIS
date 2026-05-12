d6888107c7e406ccaab6d4146cd24607ef3fba3e
liyubrt <li.yu@bluerivertech.com>
2026-05-12 09:07:25 -0700

    make two files

--- run_gpu.sh
+++ run_gpu.sh
@@ -15,19 +15,23 @@
 
 # activate virtual env
 eval "$(/home/li.yu/anaconda3/bin/conda shell.bash hook)"
-conda activate torch221_mask2former  # on lambda, for mask2former, rtdetr training
-# conda activate torch2100_mask2former  # on harvset, for mask2former, rtdetr training
+conda activate torch2100_mask2former  # for mask2former, rtdetr training
 
-# run DVIS
-cd /home/li.yu/code/DVIS
+# # train R50 based DVIS online
+# python train_net_video.py \
+#   --num-gpus 8 \
+#   --config-file ./configs/youtubevis_2022/DVIS_Online_R50.yaml \
+#   MODEL.WEIGHTS ./pretrained_models/minvis_ytvis21_R50.pth \
+#   OUTPUT_DIR /mnt/data2/jupiter/li.yu/exps/driveable_terrain_model/ytvis2022_coco_dvis_m2f_r50_0509
 
-# use R50
-python train_net_video.py \
-  --num-gpus 2 \
-  --config-file /home/li.yu/code/DVIS/configs/youtubevis_2022/DVIS_Online_R50.yaml \
-  --resume MODEL.WEIGHTS /home/li.yu/code/DVIS/pretrained_models/minvis_ytvis21_r50.pth
+# # Finetune MinVIS segmenter on COCO + YTVIS 2022
+# cd /home/li.yu/code/mymnt/DVIS
+# python train_net_video.py \
+#   --num-gpus 4 \
+#   --config-file /home/li.yu/code/mymnt/DVIS/configs/youtubevis_2022/swin/MinVIS_SwinL.yaml \
+#   OUTPUT_DIR /mnt/data2/jupiter/li.yu/exps/driveable_terrain_model/ytvis2022_coco_m2f_swl_ft_0506
 
-# # use swin large
+# # train Swin-L based DVIS online using finetuned segmenter
 # python train_net_video.py \
 #   --num-gpus 4 \
 #   --config-file /home/li.yu/code/mymnt/DVIS/configs/youtubevis_2022/swin/DVIS_Online_SwinL.yaml \
@@ -45,11 +49,14 @@ python train_net_video.py \
 #   MODEL.RFDETR.PRETRAIN_WEIGHTS /mnt/data2/jupiter/li.yu/exps/driveable_terrain_model/ytvis2022_coco_rfdetr_sl_ft_0506/rfdetr_finetuned.pt \
 #   OUTPUT_DIR /mnt/data2/jupiter/li.yu/exps/driveable_terrain_model/ytvis2022_coco_dvis_rfdetr_sl_0508
 
-# # use rfdetr
-# python train_net_video.py \
-#   --num-gpus 4 \
-#   --config-file configs/youtubevis_2022/rfdetr/DVIS_Online_RFDETR.yaml \
-#   SOLVER.IMS_PER_BATCH 64
+
+# convert brt sequence dataset to ytvis format
+python convert_custom_to_ytvis.py \
+  --input-dir /mnt/data3/jupiter/datasets/sequence_data/humans-on_path_forward-day-sequences-core-jupiter_rev11_8338/raw_seqs \
+  --output-dir /mnt/data3/jupiter/datasets/sequence_data/humans-on_path_forward-day-sequences-core-jupiter_rev11_8338 \
+  --split test \
+  --symlink
+
 
 # deactivate virtual env
 conda deactivate
